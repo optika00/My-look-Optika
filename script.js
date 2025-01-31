dd12fe12db65e6c3adc33ea512a544aa6d5ddc75
My look Optika <99coaloks@gmail.com>
2025-01-31 18:33:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Možete dodati JavaScript za interaktivnost, na primer:
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
